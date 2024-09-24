e60c38b30a7ca1d38317eac72501df284976ab62
Ufuoma-Onakpoyan <164772830+Ufuoma-Onakpoyan@users.noreply.github.com>
2024-09-24 16:11:45 +0100

    Update Customizer.jsx

--- client/src/Pages/Customizer.jsx
+++ client/src/Pages/Customizer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
 import config from '../config/config';
-import state from '../Store';
+import state from '../Store/Index';
 import { download, logoShirt, stylishShirt } from "../assets"
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
